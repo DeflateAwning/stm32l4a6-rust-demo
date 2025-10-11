a5022a4b8b6d3fb3f9b9e94a54007322cef04dea
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-10-10 18:59:29 -0600

    feat: add debug print messages

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,17 @@ use cortex_m_rt::entry;
 
 use panic_halt as _;
 
+use rtt_target::{rprintln, rtt_init_print};
 
 
 #[entry]
 fn entry_point() -> ! {
-    loop {}
+    rtt_init_print!();
+    rprintln!("Hello, world!");
+    let mut i = 0;
+    loop {
+        i += 1;
+        rprintln!("i = {}", i);
+        cortex_m::asm::delay(8_000_000);
+    }
 }
