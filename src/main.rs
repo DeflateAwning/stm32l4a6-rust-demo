7b6977be4fa28d22d37c2e26a2990e98ddbb4714
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-10-10 19:30:11 -0600

    add panic handler, switch to timer for delay

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 extern crate cortex_m;
 use cortex_m_rt::entry;
 
-use panic_halt as _;
+use core::panic::PanicInfo;
 
 use stm32l4xx_hal::{self as hal};
 use stm32l4xx_hal::prelude::*;
@@ -16,8 +16,16 @@ fn entry_point() -> ! {
     rtt_init_print!();
     rprintln!("rtt_init_print() done.");
 
+    let cortex_peripherals = cortex_m::Peripherals::take().unwrap();
     let peripheral = hal::stm32::Peripherals::take().unwrap();
     let mut rcc = peripheral.RCC.constrain();
+    let mut flash = peripheral.FLASH.constrain();
+    let mut pwr = peripheral.PWR.constrain(&mut rcc.apb1r1);
+    let clocks = rcc.cfgr.sysclk(64.MHz()).freeze(&mut flash.acr, &mut pwr);
+    rprintln!("Clocks are configured.");
+
+    let mut timer = hal::delay::Delay::new(cortex_peripherals.SYST, clocks);
+
     // let mut gpioa = peripheral.GPIOA.split(&mut rcc.ahb2);
     let mut gpioc = peripheral.GPIOC.split(&mut rcc.ahb2);
 
@@ -34,6 +42,14 @@ fn entry_point() -> ! {
 
         i += 1;
         rprintln!("i = {}", i);
-        cortex_m::asm::delay(4_000_000);
+        timer.delay_ms(500_u16);
     }
+}
+
+
+#[inline(never)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    rprintln!("{}", _info);
+    loop {}
 }
