e720ad08e59c4579ec13d915a5afc3e5d4bb4764
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-10-10 19:24:58 -0600

    switch from pac to hal (blink LED)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use cortex_m_rt::entry;
 
 use panic_halt as _;
 
-use stm32l4::stm32l4x6 as pac;
+use stm32l4xx_hal::{self as hal};
+use stm32l4xx_hal::prelude::*;
 
 use rtt_target::{rprintln, rtt_init_print};
 
@@ -13,22 +14,23 @@ use rtt_target::{rprintln, rtt_init_print};
 #[entry]
 fn entry_point() -> ! {
     rtt_init_print!();
+    rprintln!("rtt_init_print() done.");
 
-    let peripheral = pac::Peripherals::take().unwrap();
-    let _gpioa = &peripheral.GPIOA;
-    let gpioc = &peripheral.GPIOC;
-    let rcc = &peripheral.RCC;
+    let peripheral = hal::stm32::Peripherals::take().unwrap();
+    let mut rcc = peripheral.RCC.constrain();
+    // let mut gpioa = peripheral.GPIOA.split(&mut rcc.ahb2);
+    let mut gpioc = peripheral.GPIOC.split(&mut rcc.ahb2);
 
-    // Enable GPIO-C clock.
-    rcc.ahb2enr.modify(|_, w| w.gpiocen().set_bit());
-    // Set PC7 as output.
-    gpioc.moder.modify(|_, w| w.moder7().output());
+    let mut led = gpioc.pc7.into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
+
+    // TODO: Read button
+
 
     rprintln!("Hello, world!");
     let mut i = 0;
     loop {
         // Toggle PC7.
-        gpioc.odr.modify(|_, w| w.odr7().bit(i % 2 == 0));
+        led.toggle();
 
         i += 1;
         rprintln!("i = {}", i);
